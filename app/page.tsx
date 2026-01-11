0aea7924f2eea11d3125581c11f76b78169dfd37
Oluwadarasimi Omojola <omojoladarasimi9@gmail.com>
2026-01-11 18:47:03 +0100

    add Post type and fetchPosts function to retrieve posts from API

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+async function fetchPosts(): Promise<Post[]> {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return response.json();
+}
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
-    </div>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
   );
 }
